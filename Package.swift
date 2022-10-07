cb8580c887cd7ebed598d93cc1d65537b5d69f50
Andy Roth <andy.roth@ouraring.com>
2022-10-06 20:47:27 -0700

    Update package, product, and target names to SegmentSwift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Segment",
+    name: "SegmentSwift",
     platforms: [
         .macOS("10.15"),
         .iOS("13.0"),
@@ -14,8 +14,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Segment",
-            targets: ["Segment"]),
+            name: "SegmentSwift",
+            targets: ["SegmentSwift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -30,10 +30,9 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Segment",
-            dependencies: ["Sovran"]),
-        .testTarget(
-            name: "Segment-Tests",
-            dependencies: ["Segment"]),
+            name: "SegmentSwift",
+            dependencies: ["Sovran"],
+            path: "Sources/Segment"
+        )
     ]
 )
